cba7f0d9202bec96020af5d3fdad23757bdd90c0
marymirza-lad <82551682+marymirza-lad@users.noreply.github.com>
2021-06-28 17:53:34 -0700

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,25 @@
+class Paper{
+    constructor(x, y,r) {
+        var options = {
+            'isStatic':false,
+            'restitution':0.3,
+            'friction':0,
+            'density':1.2
+        }
+        this.body = Bodies.circle(x, y,r,options);
+        this.r = r;
+        this.x = x;
+        this.y = y;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r, this.r);
+        pop();
+      }
+}
